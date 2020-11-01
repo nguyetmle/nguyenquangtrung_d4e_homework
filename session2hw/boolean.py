37a574af0dfe993fb942957fa47c0f440cb79c51
quangtrung108 <tenlagi32@gmail.com>
2020-11-01 19:37:32 +0700

    boolean.py

---
+++ session2hw/boolean.py
@@ -0,0 +1,8 @@
+#A Boolean value is either true or false.
+print(type(5<6))
+print(type(5>6))
+age=int(input("enter your age ?"))
+if age>16:
+    print("Welcome!")
+else:
+    print("No, you are kid")
---
+++ session2hw/nested_conditionals.py
@@ -0,0 +1,8 @@
+name=""
+if name == "Anh Quan":
+    print("Hand some")
+elif name == "Minh Duc":
+    even_more_handsome = True
+else:
+    # webbrowser.open(“https://www.youtube.com/watch?v=04854XqcfCY”)
+    print("1")
