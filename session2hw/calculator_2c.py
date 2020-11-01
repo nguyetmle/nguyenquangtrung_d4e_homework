d3d9d22d2713b68dba1ee65365c97e6df87fe69d
quangtrung108 <tenlagi32@gmail.com>
2020-11-01 22:36:49 +0700

    session2hw

--- session2hw/calculator_2c.py
+++ session2hw/calculator_2c.py
@@ -1,5 +1,16 @@
 n=int(input('enter a number n : '))
 for row in range(1,n+1):
     for col in range(1,n+1):
-        print(row*col, end="\t")
+        if row>col:
+            n1=int(row-col)
+            if n1%2==0:
+                print(1,end=' ')
+            else:
+                print(0,end=' ')
+        else:
+            n1=int(col-row)
+            if n1%2==0:
+                print(1,end=' ')
+            else:
+                print(0,end=' ')
     print()
